99fab65a549185fdea445b3c35e04d1a67239cba
Ильшат Гибадуллин <f.merten@mail.ru>
2023-07-01 15:15:30 +0300

    version 0.2

--- core_bot.py
+++ core_bot.py
@@ -4,6 +4,7 @@
 import os
 import telebot
 from tinkoff.invest.sandbox.client import SandboxClient
+from tinkoff.invest.schemas import MoneyValue
 
 API_TOKEN = os.environ['TELE_TOKEN']     # Токен бота
 TOKEN = os.environ['TINKOFF_TOKEN']      # Токен тинькофф-инвестиций
@@ -26,19 +27,13 @@ Hi there, I am TraderBot.
 I can help you to deal with shares, bonds and other. To get info about account, send \"info\"\
 """)
 
-"""
-# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-@bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
-"""
 
 # Получение информации о счете в тинькофф-песочнице
 @bot.message_handler(commands=['info'])
 def get_info_accountant(message):
     client_info = None
     message_text = ""
-    with SandboxClient(TOKEN) as client:   # Создаем аккаунт тинькофф-инвестиций
+    with SandboxClient(TOKEN) as client:          # Запускаем клиент тинькофф-песочницы
         client_info = client.users.get_info()     # получаем информацию о счете
 
     if not client_info.prem_status:
@@ -56,7 +51,7 @@ def get_info_accountant(message):
         if name in figi.keys():
             message_text += figi[name] + '\n'
 
-    bot.send_message(message.chat.id, message_text)
+    bot.send_message(message.chat.id, message_text)         # Отправка сообщения в чат
 
 """ Открытие счета в песочнице """
 @bot.message_handler(commands=['open'])
@@ -68,7 +63,7 @@ def open_account(message):
         bot.send_message(message.chat.id, message_text)
         return
 
-    with SandboxClient(TOKEN) as client:                               # Создаем аккаунт тинькофф-инвестиций
+    with SandboxClient(TOKEN) as client:                               # Запускаем клиент тинькофф-песочницы
         sbAccountRescponse = client.sandbox.open_sandbox_account()     # создаем счет в песочнице
         sandbox_account_flag = True                                    # устанавливаем флаг создания песочницы
         message_text = "Вы создали новый счет в песочнице!\nЕго номер: " + sbAccountRescponse.account_id
@@ -80,8 +75,75 @@ def open_account(message):
         SanboxClients.close()
 
 
+""" Пополнение счета в песочнице """
 @bot.message_handler(commands=['PayIn'])
-def pay_in(message):
+def payIn(message):
+    account_sb_id = ""
+    words = message.text.split(' ')        # Разделяем текст строки на слова
+    amount = int(words[-1])
+
+    """ Пополнение счета в песочнице """
+    with SandboxClient(TOKEN) as client:
+        accounts_info = client.users.get_accounts()          # получаем информацию о всех счетах
+        account_sb_id = str(accounts_info.accounts[0].id)    # Получаем первый счет из списка
+
+        # Получаем целую и дробную часть суммы
+        unit_sum = int(amount)
+        nano_sum = 0
+        if amount % 1 != 0:
+            nano_sum = int((str(amount % 1))[2:11])
+
+        pay_sum = MoneyValue(currency="RUB", units=unit_sum, nano=nano_sum)                          # Преобразование суммы к нужному типу
+        client.sandbox.sandbox_pay_in(account_id=account_sb_id, amount=pay_sum)                      # Пополнение счета на сумму pay_sum
+        bot.send_message(message.chat.id, f"Account {account_sb_id} was refilled by {amount} RUB")   # Высылаем в чат подтверждение пополнения
+
+
+""" Получение всех аккаунтов в песочнице """
+@bot.message_handler(commands=['accounts'])
+def getAccounts(message):
+    with SandboxClient(TOKEN) as client:                    # Запускаем клиент тинькофф-песочницы
+        message_text = ""
+        accounts_info = client.users.get_accounts()         # получаем информацию о счете
+
+        for account in accounts_info.accounts:
+            message_text += str(account) + "\n\n"
+
+        while(message_text[-1] == '\n'):
+            message_text = message_text[:-1]
+
+        bot.send_message(message.chat.id, message_text)
+
+
+# Получаем баланс счета в песочнице по id
+@bot.message_handler(commands=['portfolio'])
+def getSandboxPortfolio(message):
+    words = message.text.split(' ')
+    in_account_id = words[-1]         # Введенный id
+
+    with SandboxClient(TOKEN) as client:             # Запускаем клиент тинькофф-песочницы
+        accounts_info = client.users.get_accounts()  # получаем информацию о счете
+        isNotAccount = True
+
+        # Проверка наличия счета в списке
+        for account in accounts_info.accounts:
+            if in_account_id == str(account.id):     # Если нашли нужный счет, то выходим из списка
+                isNotAccount = False
+                break
+
+        # Если счета нет, то выводим сообщение об ошибке и выходим из функции
+        if isNotAccount:
+            bot.send_message(message.chat.id, "Неверно указан id счета")
+            return
+
+        # Формулировка сообщения
+        message_text = f"Баланс счета {in_account_id}: \n"
+        portfolio = client.sandbox.get_sandbox_portfolio(account_id=in_account_id)
+        total_amount = portfolio.total_amount_portfolio
+        message_text += "Currency: " + total_amount.currency + "\n"
+        message_text += "Units: " + str(total_amount.units) + "\n"
+        message_text += "Nano: " + str(total_amount.nano)
+
+        bot.send_message(message.chat.id, message_text)   # Отправляем состояние счета
 
 if __name__ == '__main__':
     bot.infinity_polling()
